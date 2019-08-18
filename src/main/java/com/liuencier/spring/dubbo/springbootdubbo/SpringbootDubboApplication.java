e7733d84d5bba6c05d538e37f3e504611f768973
liuenci <154910381@qq.com>
2019-08-18 12:36:37 +0800

    init

---
+++ src/main/java/com/liuencier/spring/dubbo/springbootdubbo/SpringbootDubboApplication.java
@@ -0,0 +1,13 @@
+package com.liuencier.spring.dubbo.springbootdubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDubboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDubboApplication.class, args);
+    }
+
+}
