b97e8a5371ef0662d788fee3e7121315d53bb7bf
liuenci <154910381@qq.com>
2019-08-18 12:53:09 +0800

    新增服务接口实现类

---
+++ provider/src/main/java/com/liuencier/springboot/dubbo/provider/quickstart/QuickStartServiceImpl.java
@@ -0,0 +1,14 @@
+package com.liuencier.springboot.dubbo.provider.quickstart;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.liuencier.springboot.dubbo.provider.ServiceAPI;
+import org.springframework.stereotype.Component;
+
+@Component
+@Service
+public class QuickStartServiceImpl implements ServiceAPI {
+    @Override
+    public String sendMessage(String message) {
+        return "quickstart-provider-message="+message;
+    }
+}
